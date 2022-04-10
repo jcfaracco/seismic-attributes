465c00cf3a9c20b5064e851f5ac8f4e6d8b8f48f
Julio Faracco <jcfaracco@gmail.com>
2022-04-10 00:58:02 -0300

    Fixing differences between dask and cupy
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- d2geo/attributes/LBP.py
+++ d2geo/attributes/LBP.py
@@ -255,8 +255,12 @@ class LBPAttributes(BaseAttributes):
                     int(np.ceil(dimx/block_size)),)
             block = (block_size, block_size, block_size,)
 
-            __lbp_gpu(grid, block, (inp, out, cp.float32(max_local.get()),
-                                    cp.float32(min_local.get()), cp.int32(dimx),
+            if not isinstance(darray, da.core.Array):
+                max_local = max_local.get()
+                min_local = min_local.get()
+
+            __lbp_gpu(grid, block, (inp, out, cp.float32(max_local),
+                                    cp.float32(min_local), cp.int32(dimx),
                                     cp.int32(dimy), cp.int32(dimz)))
 
             unique_array = cp.unique(out)
