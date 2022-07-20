9f0d3fa3ecb15fc2e053aa6c83fb2f1c5b8e6737
Julio Faracco <jcfaracco@gmail.com>
2022-07-19 23:28:22 -0300

    Adding more tests to generic test case
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -55,14 +55,17 @@ def parameterize_all_methods_attributes():
     
     functions = []
     for cls in classes:
-        obj = eval(cls)()
-        funcs = [func for func in dir(obj) if callable(getattr(obj, func)) and \
-                                              not func.startswith("_") and \
-                                              func != "create_array" and \
-                                              func != "set_cuda"]
+        obj_default = eval(cls)()
+        obj_np = eval(cls)(use_cuda=False)
+        obj_cp = eval(cls)(use_cuda=True)
+
+        funcs = [func for func in dir(obj_default) if callable(getattr(obj_default, func)) and \
+                                                      not func.startswith("_") and \
+                                                      func != "create_array" and \
+                                                      func != "set_cuda"]
         for func in funcs:
             if func not in block_shape_list:
-                functions.append({"obj": obj, "func": func})
+                functions.append({"obj_default": obj_default, "obj_np": obj_np, "obj_cp": obj_cp, "func": func})
 
     return functions
 
@@ -75,7 +78,7 @@ class TestShapeAttributes(TestCase):
             return string[len(prefix):]
         return string
 
-    def test_shape_check(self):
+    def test_shape_check_from_array(self):
         in_shape = (20, 20, 20)
 
         if util.is_cupy_enabled():
@@ -85,7 +88,7 @@ class TestShapeAttributes(TestCase):
             rng = np.random.default_rng(seed=42)
             in_data = rng.random(in_shape)
 
-        func = getattr(self.obj, self.func)
+        func = getattr(self.obj_default, self.func)
 
         try:
             out_data = func(in_data).compute()
@@ -93,6 +96,96 @@ class TestShapeAttributes(TestCase):
             return
 
         self.assertEqual(in_shape, out_data.shape)
+
+    def test_shape_check_from_dask_array(self):
+        in_shape = (20, 20, 20)
+        in_shape_chunks = (5, 5, 5)
+
+        if util.is_cupy_enabled():
+            rng = cp.random.default_rng(seed=42)
+            in_data = rng.random(in_shape)
+        else:
+            rng = np.random.default_rng(seed=42)
+            in_data = rng.random(in_shape)
+
+        # The input data is small, so we can import from array
+        in_data = da.from_array(in_data, chunks=in_shape_chunks)
+
+        func = getattr(self.obj_default, self.func)
+
+        try:
+            out_data = func(in_data).compute()
+        except NotImplementedError:
+            return
+
+        self.assertEqual(in_shape, out_data.shape)
+
+    def test_dtype_as_np_from_dask_array(self):
+        in_shape = (20, 20, 20)
+        in_shape_chunks = (5, 5, 5)
+
+        rng = np.random.default_rng(seed=42)
+        in_data = rng.random(in_shape)
+
+        # The input data is small, so we can import from array
+        in_data = da.from_array(in_data, chunks=in_shape_chunks)
+
+        func = getattr(self.obj_np, self.func)
+
+        try:
+            out_data = func(in_data)
+            out_data_comp = out_data.compute()
+        except NotImplementedError:
+            return
+
+        self.assertEqual(out_data.dtype, out_data_comp.dtype)
+
+    @unittest.skipIf(not util.is_cupy_enabled(),
+                     "not supported CUDA in this platform")
+    def test_dtype_as_cp_from_dask_array(self):
+        in_shape = (20, 20, 20)
+        in_shape_chunks = (5, 5, 5)
+
+        rng = cp.random.default_rng(seed=42)
+        in_data = rng.random(in_shape)
+
+        # The input data is small, so we can import from array
+        in_data = da.from_array(in_data, chunks=in_shape_chunks)
+
+        func = getattr(self.obj_cp, self.func)
+
+        try:
+            out_data = func(in_data)
+            out_data_comp = out_data.compute()
+        except NotImplementedError:
+            return
+
+        self.assertEqual(out_data.dtype, out_data_comp.dtype)
+
+    @unittest.skipIf(not util.is_cupy_enabled(),
+                     "not supported CUDA in this platform")
+    def test_compare_attributes_cross_platforms(self):
+        in_shape = (20, 20, 20)
+        in_shape_chunks = (5, 5, 5)
+
+        rng = cp.random.default_rng(seed=42)
+        in_data_cp = rng.random(in_shape)
+        in_data_np = in_data_cp.copy().get()
+
+        # The input data is small, so we can import from array
+        in_data_cp = da.from_array(in_data_cp, chunks=in_shape_chunks)
+        in_data_np = da.from_array(in_data_np, chunks=in_shape_chunks)
+
+        func_cp = getattr(self.obj_cp, self.func)
+        func_np = getattr(self.obj_np, self.func)
+
+        try:
+            out_data_cp = func_cp(in_data_cp)
+            out_data_np = func_np(in_data_np)
+        except NotImplementedError:
+            return
+
+        self.assertEqual(out_data_np.compute(), out_data_cp.compute().get())
 
 
 def parameterize_class_attributes():
